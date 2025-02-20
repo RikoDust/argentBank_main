189119b356de06dc77adb816c78b49b485399d1b
Emeric <emeric.pataut@gmail.com>
2025-02-20 17:53:09 +0100

    chore(init): initialize React project with Vite

---
+++ argentBankFront/src/RoutesComponent.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+
+// COMPONENTS
+
+
+
+// PAGES
+
+
+
+
+
+
+function RoutesComponent() {
+    return(
+        <Router>
+                
+            <Routes>
+                <Route />
+            </Routes>
+            
+        </Router>
+    );
+}
+
+
+
+
+
+
+export default RoutesComponent;
