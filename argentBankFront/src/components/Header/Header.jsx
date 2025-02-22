f86e7c689333cf4907572cd7b8cd0a7d8d0ee62b
Emeric <emeric.pataut@gmail.com>
2025-02-22 17:32:19 +0100

    feat(img): convert images to WebP format

--- argentBankFront/src/components/Header/Header.jsx
+++ argentBankFront/src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = ({ isAuthenticated, username, onLogout }) => {
   return (
     <nav className="main-nav">
       <Link className="main-nav-logo" to="/">
-        <img className="main-nav-logo-image" src="src/assets/img/argentBankLogo.png" alt="Argent Bank Logo" />
+        <img className="main-nav-logo-image" src="src/assets/img/argentBankLogo.webp" alt="Argent Bank Logo" />
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>
